ce31e9133f71e78a14a8f90ddb55fce2dcdd9e05
KeithBautista <keith1829@gmail.com>
2022-05-17 01:12:27 +0000

    Added function to allow content to show and hide based on nav buttons

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+/*Start of Show and Hide Sections*/
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
