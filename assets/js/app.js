f665eedb7b52d1d9a3dbe53fa33f6b8fefd558e9
KeithBautista <keith1829@gmail.com>
2022-05-18 08:46:33 +0000

    created function to clearStatusClass

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,9 +66,10 @@ function setStatusClass(element, correct){
     }
 }
 
-
-
-
+ function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
 const questions = [
     {
